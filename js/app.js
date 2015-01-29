933d93e7d80677b88626777cc741a0fc6103b2d8
Tyler Johnson <tylernj42@gmail.com>
2015-01-29 17:01:03 -0600

    Added basic styles

--- js/app.js
+++ js/app.js
@@ -3,19 +3,31 @@
 	var app;
 	app = angular.module('activityList', ['ngRoute']);
 	
+	/* Global Data.  Pulls every 5 minutes */
+	app.controller('ListController', function($scope, $rootScope){
+		var list = this;
+		list.activities = $rootScope.data.activities;
+		$scope.Initialize = function (){
+			$rootScope.$watch('data', function(newValue, oldValue){
+				if (newValue !== oldValue){
+					list.activities = newValue.activities;
+				}
+			});
+		};
+	
+		$scope.Initialize();
+	});
+	
 	/* Define pages and templates using routes */
 	app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
 		$routeProvider
-			.when('/', {templateUrl : 'partials/home.html', controller : 'ListController'})
+			.when('/', {templateUrl : 'partials/home.html'})
 			.when('/:permalink', {templateUrl : 'partials/activity.html', controller : 'ActivityController'})
 			.otherwise({redirectTo : '/'});
 		$locationProvider.html5Mode(true);
 	}])
 	
-	app.controller('HomeController', function($http){
-		
-	});
-	
+	/* Controller for individual activity pages */
 	app.controller('ActivityController', function($scope, $rootScope, $routeParams){
 		console.log('rootScope:')
 		console.log($rootScope.data.activities);
@@ -26,29 +38,13 @@
 		$scope.activity = activity[0];
 	});
 	
-	/* Controller for lists of activities */
 	
-	
-	
-	
-	
-	app.run(function Poller($http, $interval, $rootScope){
-		$rootScope.data = {'slider' : [], 'activities' : []};
-	
-		var getData = function() {
-			$http.get('http://tnjdesigns.com/sandbox/pbl-2015/app-api/')
-				.then(function(response){
-					$rootScope.data = response.data;
-				});
-		};
-		getData();
-	
-		var loopGetData = function() {
-			$interval(getData, 5000);
-		};
-		loopGetData();
+	/* Controller for slider on home page */
+	app.controller('SliderController', function($http){
+		
 	});
 	
+	/* Controller for lists of activities on home page */
 	app.controller('ListController', function($scope, $rootScope){
 		var list = this;
 		list.activities = $rootScope.data.activities;
