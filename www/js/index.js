a8ddfcb1eaac59fc23c9632933dc2bfd05d7a9b3
Tyler Johnson <tylernj42@gmail.com>
2015-02-16 22:44:35 -0600

    Removed default code

--- www/js/index.js
+++ www/js/index.js
@@ -4,19 +4,6 @@ var app = {
     },
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
     }
 };
 
